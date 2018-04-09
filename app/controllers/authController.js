1aefebfbbcd7d1353c8f46233b411dba7f7a5816
gu1ma <gabrielgs1408@gmail.com>
2018-04-08 23:03:05 -0300

    [ADD] Recuperacao de senha
    Adicionando funcionalidade de recuperacao de senha enviando um token de autenticacao por email

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
-
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const authConfig = require('../../config/auth');
+const crypto = require('crypto');
+const mailer = require('../../modules/mailer');
 
 
 function generateToken(params = {}){
@@ -45,6 +46,81 @@ router.post('/authenticator', async (req, res) => {
 	//const token = ;
 
 	res.send({user, token: generateToken({id: user.id})});
+
+});
+
+router.post('/forgot-password', async (req, res) => {
+	const { email } = req.body;
+
+	try{
+		const user = await User.findOne({ email });
+
+		if(!user)
+			return res.status(400).send({ error: 'User not found' });
+		
+		const token = crypto.randomBytes(20).toString('hex');
+
+		const now = new Date();
+		now.setHours(now.getHours() + 1);
+
+		await User.findByIdAndUpdate(user.id, {
+			'$set':{
+				passwordResetToken: token,
+				passwordResetExpires: now,
+			}
+		});
+
+		//console.log(token, now);
+
+		mailer.sendMail({
+			to: email,
+			from: 'resetpass@nodeapi.com.br',
+			template: 'auth/forgotPassword',
+			context: { token }
+		}, (err) => {
+			if(err){
+				//console.log(err);
+				return res.status(400).send({ error: 'Cannot send forgot password email' });
+			}
+
+			return res.send();
+		})
+		
+	} catch(err) {
+		res.status(400).send({ error: 'Error on forgot password, try again' });
+		//console.log('erro', err);
+	}
+});
+
+router.post('/reset-password', async (req, res) => {
+	const{ email, token, password } = req.body;
+
+	try{
+		const user = await User.findOne({ email })
+			.select('+passwordResetToken passwordResetExpires');
+
+		if(!user)
+			return res.status(400).send({ error: 'User not found' });
+
+		if(token !== user.passwordResetToken)
+			return res.status(400).send({ error: 'Token invalid' });
+
+		const now = Date();
+
+		if(now > user.passwordResetExpires)
+			return res.status(400).send({ error: 'Token expired' });
+
+		user.password = password;
+
+		await user.save();
+
+		return res.send();
+
+	} catch(err) {
+		if(err)
+			res.send(400).send({ error: 'Cannot reset password, try again' });
+
+	}
 
 });
 
